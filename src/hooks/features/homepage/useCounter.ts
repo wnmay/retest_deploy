6429e1cb1cc5bdf83c06b3edf5171ab2ecafd72a
wnmay <wanathachat@gmail.com>
2025-11-06 10:58:37 +0700

    [ADD]

--- src/hooks/features/homepage/useCounter.ts
+++ src/hooks/features/homepage/useCounter.ts
@@ -4,9 +4,18 @@ export default function useCounter() {
   const [count, setCount] = useState(0);
   const [val, setVal] = useState(1);
 
-  function increment() {
-    setCount((count) => count + val);
-  }
+  const increment = () => {
+    setCount(prev => prev + val);
+  };
 
-  return { count, increment, val, setVal };
+  /* 
+  const updateVal = (newVal) => {
+    // increment one time using **old val**
+    setCount(prev => prev + val);
+    // then update val
+    setVal(newVal);
+  };*/
+
+  //return { count, val, increment, setVal: updateVal };
+  return { count, val, increment, setVal};
 }
